116ab293a798752dc2af25aee4312028063000b0
Zarina <introspkt.sound@gmail.com>
2022-08-24 23:13:18 -0400

    Form Validation Alert

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,92 +3,113 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function () {
-  // Test / driver code (temporary). Eventually will get this from the server.
+
   const tweetData = [
     {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac",
       },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants",
       },
-      created_at: 1461116232227,
+      "created_at": 1461116232227,
     },
 
     {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd",
+      "content": {
+        "text": "Je pense , donc je suis",
       },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
+      "created_at": 1461113959088,
+    }
+  ]
 
-  const createTweetElement = function (tweet) {
-    const $toDynamic = `<article class="tweet">
-<header class="tweet-header">
-  <div class="user-image">    
-  <img src="${tweetData.user.avatars}"></img>
-  <h3>${tweetData.user.name}</h3>
-  </div>
-  <p>${tweetData.user.handle}</p>
-</header>
-<p>${tweetData.content.text} </p>
-<footer class="footer">
+  $(".tweet-submit").submit(function (event) {
 
-    <p>${timeago.format(tweetData.created_at)}</p>
-    <div class="social-icons">
-      <i class="fa-solid fa-flag flag"></i>
-      <i class="fa-solid fa-retweet retweet"></i>
-      <i class="fa-solid fa-heart heart"></i>
-    </div>
-</footer>
-</article> `;
-    return $toDynamic;
+    event.preventDefault();
+    const tweetLength = $("#tweet-text").val().length;
+console.log("tweetLength", tweetLength)
+    let numsLeft = 140 - tweetLength;
+    console.log("numsLeft", numsLeft)
+    if (numsLeft < 0) {
+
+      //charCounter.addClass("tweetTooLong") 
+       alert("Tweet too long");
+    } else if (numsLeft === 140) {
+      alert("Tweet too short")
+      //charCounter.removeClass("tweetTooLong");
+    } else {
+    $.ajax("/tweets", { method: "POST" }).then(function () {
+      console.log($(this).serialize());
+
+    });
   };
+});
 
-  const renderTweets = function (tweets) {
+loadTweets()
+})
+  const renderTweets = function(tweets) {
     // loops through tweets
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
-    //const container = $("");
-    for (let tweet of tweets) {
-      const element = createTweetElement(tweet);
-      //container.append(element);
-      $("#tweets-container").append(element);
-      //console.log("container", container)
-      console.log("element", element);
+    for (const data of tweets) {
+      const $tweet = createTweetElement(data);
+      $(function() {
+        $('#tweets-container').append($tweet);
+      });
     }
+  }
+
+ /* Create the tweet element */
+  const createTweetElement = function(tweet) {
+  const $tweet = `<article>
+      <article>
+        <img src=${tweet.user.avatars} />
+        <div class="name">${tweet.user.name}</div>
+        <div class="handle">${tweet.user.handle}</div>
+      </article>
+
+      <div class="old-tweet">
+      <p>${tweet.content.text}</p>
+      </div>
+
+      <footer>
+      <div class="date-posted">
+      <p>${timeago.format(tweetData.created_at)}</p>
+    </div>
+
+        <div class="tweet-icons">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+      </article>`;
+    return $tweet;
   };
 
-  renderTweets(tweetData);
-});
+  $("#tweeter-form").on("submit", function() {
+    $(function() {
+      const $form = $("#tweeter-form");
+      $form.submit(function(event) {
+        event.preventDefault();
 
-$("#tweeter-form").on("submit", function() {
-  $(function() {
-    const $form = $("#tweeter-form");
-    $form.submit(function(event) {
-      event.preventDefault();
-
-      $.ajax({ method: "POST", url: "/tweets/", data: $(this).serialize() })
-      .then(function() {
-        console.log($(this).serialize());
+        $.ajax({ method: "POST", url: "/tweets/", data: $(this).serialize() })
+        .then(function() {
+          console.log($(this).serialize());
+        });
       });
-
     });
-  });
-});
 
-const loadTweets = function() {
-  $.get("/tweets/").then(function(data) {
-    renderTweets(data);
-  })
-}
-
+    const loadTweets = function() {
+      $.get("/tweets/").then(function(data) {
+        renderTweets(data);
+      })
+    }
+  
+  
